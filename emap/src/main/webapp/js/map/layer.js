2df9cc2067b914c7968db13fad615afb315f96a1
yunho <yunho@DESKTOP-VJ9QA1O>
2023-11-06 22:34:12 +0900

    gwc

--- emap/src/main/webapp/js/map/layer.js
+++ emap/src/main/webapp/js/map/layer.js
@@ -1,4 +1,5 @@
 var geoserverWmsUrl = "http://141.164.59.52:8089/geoserver/wms";
+var geoserverGwcUrl = "http://141.164.59.52:8089/geoserver/gwc/service/wmts";
 function wmsInit(){
 
 	// 전체 회색 - 기존 범위 외의 바다 회색 처리 위함
@@ -23,7 +24,7 @@ function wmsInit(){
 	var worldcountries = new ol.layer.Tile({
 		id : 'worldcountries',
     	title: 'worldcountries',
-    	opacity: 1,
+    	opacity: 0.5,
         source: new ol.source.TileWMS({
             url: geoserverWmsUrl,
             serverType: 'geoserver',
@@ -43,16 +44,25 @@ function wmsInit(){
 		id : 'Day',
     	title: 'Day',
     	opacity: 1,
-        source: new ol.source.TileWMS({
-            url: geoserverWmsUrl,
-            serverType: 'geoserver',
-            crossOrigin: 'anonymous',            
-            params: { 
-            	'VERSION': '1.1.0' , 
-                'LAYERS': 'Day',               
-                'CRS' : 'EPSG:3857',
-            },            
-        })
+        source: new ol.source.WMTS({
+            url: geoserverGwcUrl,
+            layer: 'Day',
+            matrixSet: 'EPSG:4326',
+            format: 'image/png',
+            projection: new ol.proj.Projection({
+            	code: 'EPSG:4326',
+            	units: 'degrees',
+            	axisOrientation: 'neu'
+            	}),
+            tileGrid: new ol.tilegrid.WMTS({
+              tileSize: [256,256],
+              extent: [-180.0,-90.0,180.0,90.0],
+              origin: [-180.0, 90.0],
+              resolutions: [0.703125, 0.3515625, 0.17578125, 0.087890625, 0.0439453125, 0.02197265625, 0.010986328125, 0.0054931640625, 0.00274658203125, 0.001373291015625, 6.866455078125E-4, 3.4332275390625E-4, 1.71661376953125E-4, 8.58306884765625E-5, 4.291534423828125E-5, 2.1457672119140625E-5, 1.0728836059570312E-5, 5.364418029785156E-6, 2.682209014892578E-6, 1.341104507446289E-6, 6.705522537231445E-7, 3.3527612686157227E-7],
+              matrixIds: ['EPSG:4326:0', 'EPSG:4326:1', 'EPSG:4326:2', 'EPSG:4326:3', 'EPSG:4326:4', 'EPSG:4326:5', 'EPSG:4326:6', 'EPSG:4326:7', 'EPSG:4326:8', 'EPSG:4326:9', 'EPSG:4326:10', 'EPSG:4326:11', 'EPSG:4326:12', 'EPSG:4326:13', 'EPSG:4326:14', 'EPSG:4326:15', 'EPSG:4326:16', 'EPSG:4326:17', 'EPSG:4326:18', 'EPSG:4326:19', 'EPSG:4326:20', 'EPSG:4326:21']
+            }),
+            wrapX: true
+          })
     });
     map.addLayer(DAY);
 
